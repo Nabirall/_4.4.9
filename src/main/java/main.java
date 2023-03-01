4dae737f90a0a71153151528f370d81cf64493a1
Nabirall <drozdover99@mail.ru>
2023-03-01 10:08:26 +0300

    1) 2)

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,6 +5,44 @@ import java.util.logging.Logger;
 public class main {
 public static void main(String[] args) {
 
+    Logger logger = Logger.getLogger(main.class.getName());
+    Inspector inspector = new Inspector();
+    Spy spy = new Spy(logger);
+    Thief thief = new Thief(10000);
+    UntrustworthyMailWorker worker = new UntrustworthyMailWorker(new MailService[]{spy, thief, inspector});
+    AbstractSendable[] correspondence = {
+            new MailMessage("Oxxxymiron", "Гнойный", " Я здесь чисто по фану, поглумиться над слабым\n" +
+                    "Ты же вылез из мамы под мой дисс на Бабана...."),
+            new MailMessage("Гнойный", "Oxxxymiron", "....Что? Так болел за Россию, что на нервах терял ганглии.\n" +
+                    "Но когда тут проходили митинги, где ты сидел? В Англии!...."),
+            new MailMessage("Жириновский", AUSTIN_POWERS, "Бери пацанов, и несите меня к воде."),
+            new MailMessage(AUSTIN_POWERS, "Пацаны", "Го, потаскаем Вольфовича как Клеопатру"),
+            new MailPackage("берег", "море", new Package("ВВЖ", 32)),
+            new MailMessage("NASA", AUSTIN_POWERS, "Найди в России ракетные двигатели и лунные stones"),
+            new MailPackage(AUSTIN_POWERS, "NASA", new Package("ракетный двигатель ", 2500000)),
+            new MailPackage(AUSTIN_POWERS, "NASA", new Package("stones ", 1000)),
+            new MailPackage("Китай", "КНДР", new Package("banned substance ", 10000)),
+            new MailPackage(AUSTIN_POWERS, "Жопа запрещенная группировка", new Package("tiny bomb", 9000)),
+            new MailMessage(AUSTIN_POWERS, "Психиатр", "Помогите"),
+    };
+
+    for (AbstractSendable p : correspondence) {
+        try {
+            print("До:  ", p);
+            Sendable sendable = worker.processMail(p);
+            print("После:  ", sendable);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "из: " + p.getFrom() + " куда: " + p.getTo() + " Содержимое: "
+                    + (p instanceof MailMessage ? ((MailMessage) p).getMessage() : ((MailPackage) p).getContent().getContent()
+                    + " Цена=" + ((MailPackage) p).getContent().getPrice()) + " Exceptions: " + e);
+        }
+    }
+}
+
+public static void print(String prefix, Sendable p) {
+    System.out.println(prefix + "из: " + p.getFrom() + " куда: " + p.getTo() + " Содержимое: "
+            + (p instanceof MailMessage ? ((MailMessage) p).getMessage() : ((MailPackage) p).getContent().getContent()
+            + " Цена=" + ((MailPackage) p).getContent().getPrice()));
 }
 
 public static final String AUSTIN_POWERS = "Austin Powers";
@@ -154,25 +192,40 @@ public static class RealMailService implements MailService {
     }
 }
 
-//public static class UntrustworthyMailWorker implements MailService {
-//    private RealMailService realMailService;
-//    private MailService[] mailServices;
-//
-//    public UntrustworthyMailWorker(MailService[] mailServices) {
-//
-//    }
-//
-//    @Override
-//    public Sendable processMail(Sendable mail) {
-//       RealMailService.processMail();
-//        }
-//        return m;
-//    }
-//
-//    public RealMailService getRealMailService() {
-//        return realMailService;
-//    }
-//}
+public static class UntrustworthyMailWorker implements MailService {
+    private RealMailService realMailService;
+    private MailService[] mailServices;
+
+    public UntrustworthyMailWorker(MailService[] mailServices) {
+        this.mailServices = mailServices;
+        for (MailService ms : this.mailServices) {
+            if (ms instanceof RealMailService)
+                this.realMailService = (RealMailService) ms;
+        }
+        if (this.realMailService == null) {
+            this.realMailService = new RealMailService();
+        }
+    }
+
+    @Override
+    public Sendable processMail(Sendable mail) throws Exception {
+        Sendable m = mail;
+        for (MailService ms : this.mailServices) {
+            if (ms instanceof RealMailService)
+                continue;
+            m = ms.processMail(m);
+        }
+        if (getRealMailService() != null) {
+            m = this.realMailService.processMail(m);
+        }
+        return m;
+    }
+
+
+    public RealMailService getRealMailService() {
+        return realMailService;
+    }
+}
 
 public static class Spy implements MailService {
     private final static Logger LOGGER = Logger.getLogger(Spy.class.getName());
@@ -214,11 +267,11 @@ public static class Thief implements MailService {
 
     @Override
     public Sendable processMail(Sendable mail) {
-        if (mail instanceof Package) {
-            Package pg = (Package) mail;
-            if (pg.getPrice() >= cost) {
-                summ += pg.getPrice();
-                mail = new MailPackage(mail.getFrom(),mail.getTo(),new Package("stones instead of " + pg.getContent(), 0));
+        if (mail instanceof MailPackage) {
+            MailPackage pg = (MailPackage) mail;
+            if (pg.getContent().getPrice() >= cost) {
+                this.summ += pg.getContent().getPrice();
+                return new MailPackage(mail.getFrom(), mail.getTo(), new Package("stones instead of " + pg.getContent().getContent(), 0));
 
             }
         }
@@ -235,14 +288,15 @@ public static class Inspector implements MailService {
     @Override
     public Sendable processMail(Sendable mail) throws RuntimeException {
         if (mail instanceof Package) {
-            Package pg = (Package) mail;
+            MailPackage pg = (MailPackage) mail;
 
-            if (pg.getContent().equals("weapons") || pg.getContent().equals("banned substance"))
+            if (pg.getContent().equals(WEAPONS) || pg.getContent().equals(BANNED_SUBSTANCE))
                 throw new IllegalPackageException();
             if (pg.getContent().equals("stones"))
                 throw new StolenPackageException();
-            if (pg.getPrice() == 0)
-                throw new RuntimeException();
+            if (pg.getContent().getPrice() == 0)
+                System.out.println("AAAAAAAAAAAA");
+                throw new RuntimeException("посылка обворована");
         }
         return mail;
     }
